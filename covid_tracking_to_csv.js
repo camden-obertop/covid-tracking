4d289804fb830cd5cac1c44471662843f3d86177
camden-obertop <52179320+camden-obertop@users.noreply.github.com>
2020-03-31 22:35:34 -0500

    Add files via upload

---
+++ covid_tracking_to_csv.js
@@ -0,0 +1,43 @@
+const fetch = require("node-fetch")
+const fs = require("fs");
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+const date = new Date();
+
+const csvWriter = createCsvWriter({
+    path: `${date.getMonth() + 1}-${date.getDate()}.csv`,
+    header: [
+        {id: 'state', title: 'State'},
+        {id: 'positive', title: 'Positive'},
+        {id: 'negative', title: 'Negative'},
+        {id: 'pending', title: 'Pending'},
+        {id: 'hospitalized', title: 'Hospitalized'},
+        {id: 'deaths', title: 'Deaths'}
+    ]
+})
+
+async function getData() {
+    let response = await fetch("https://covidtracking.com/api/states");
+    let data = await response.json()
+    return data;
+}
+
+const total_data = []
+
+getData().then(data => {
+    for (const state_or_territory of data) {
+        console.log("a");
+        total_data.push({
+            state: state_or_territory.state, 
+            positive: state_or_territory.positive, 
+            negative: state_or_territory.negative, 
+            pending: state_or_territory.pending, 
+            hospitalized: state_or_territory.hospitalized, 
+            deaths: state_or_territory.death
+        });
+    }
+
+    console.log(total_data);
+
+    csvWriter.writeRecords(total_data).then(() => console.log("File Successfully Written!"));
+});
